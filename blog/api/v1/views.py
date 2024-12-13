1bd8e22036449750e7185c8f1373efa5a6b7e9f7
shahramsamar <shahramsamar2010@gmail.com>
2024-12-13 09:41:16 +0330

    completed post list

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -10,10 +10,13 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def post_list(request):
-    return Response("ok")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
 
 @api_view()
 def post_detail(request, id):
-    post = get_object_or_404(Post, pk=id)
+    post = get_object_or_404(Post, pk=id, status=True)
     serializer = PostSerializer(post)
     return Response(serializer.data)
