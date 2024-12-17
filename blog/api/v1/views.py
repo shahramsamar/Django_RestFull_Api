0134387fdda00072101d07182e94fb2e5d23c992
shahramsamar <shahramsamar2010@gmail.com>
2024-12-17 20:41:16 +0330

    permission for login and see post

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,14 +1,15 @@
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from blog.api.v1.serializer import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
- 
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 
 
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def post_list(request):
     # Handle GET request
     if request.method == 'GET':
@@ -18,7 +19,7 @@ def post_list(request):
         serializer = PostSerializer(posts, many=True)
         # Return serialized data as response
         return Response(serializer.data)
-    
+
     # Handle POST request
     elif request.method == 'POST':
         # return Response("ok")
@@ -28,43 +29,44 @@ def post_list(request):
         serializer = PostSerializer(data=request.data)
         # Validate the serializer data
         if serializer.is_valid(raise_exception=True):
-        # Save the new post
+            # Save the new post
             serializer.save()
         # Return the serialized data of the new post
             return Response(serializer.data)
 
 
-@api_view(['GET','PUT','DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def post_detail(request, id):
-   
+
     # Get the post object or return a 404 error if not found
-    post = get_object_or_404(Post, pk=id, 
-                                        status=True)
+    post = get_object_or_404(Post, pk=id,
+                             status=True)
 
     # Handle GET request
     if request.method == 'GET':
 
         # Serialize the post object
         serializer = PostSerializer(post)
-        
+
         # Return the serialized data as a response
         return Response(serializer.data)
-        
+
         # Handle PUT request
     elif request.method == 'PUT':
         # Create a serializer instance with the request data
         serializer = PostSerializer(post,
-         data=request.data)
+                                    data=request.data)
         # Validate the serializer data
         if serializer.is_valid(raise_exception=True):
-        # Save the new post
+            # Save the new post
             serializer.save()
         #  Return the serialized data of the new post
         return Response(serializer.data)
 
      # Handle DELETE request
-    elif request.method == 'DELETE':  
-        post.delete()  
+    elif request.method == 'DELETE':
+        post.delete()
         #  Return the serialized data of the new post
-        return Response({'detail':'item removed successfully'}, 
-                                                                 status=status.HTTP_204_NO_CONTENT)
+        return Response({'detail': 'item removed successfully'},
+                        status=status.HTTP_204_NO_CONTENT)
